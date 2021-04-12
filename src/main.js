a94cfaf48093e96ab55cc20f8da9e3905b00b5a3
Angelica <atheran082@gmail.com>
2021-04-12 11:15:57 -0500

    maquetado pagina inicio login

--- src/main.js
+++ src/main.js
@@ -3,3 +3,17 @@
 import { myFunction } from './lib/index.js';
 
 myFunction();
+
+const formulario = document.createElement ('form');
+
+const html = `
+<div> <input type="email" class="usuario" placeholder="Ingrese email" /><div/>
+<div><input type="password" class="contraseña" placeholder="Ingrese contraseña"/><div/>
+
+<div><button class="btnI">Ingresar</button><div/>
+<div><button class="btnR">Registrate</button><div/>
+
+`
+formulario.innerHTML = html;
+
+document.getElementById('main').appendChild(formulario);
