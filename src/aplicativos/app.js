d5a08e405de18c6e00bee43f3d844cd753ef8ec5
AngelicaT <76055451+AngelicaT319@users.noreply.github.com>
2021-04-29 06:09:32 -0500

    revisando cambios

--- src/aplicativos/app.js
+++ src/aplicativos/app.js
@@ -1,26 +1,23 @@
-// LOGIN CON GOOGLE
-export function registroGoogle() {
-  const provider = new firebase.auth.GoogleAuthProvider();
-  firebase.auth().signInWithRedirect(provider);
-}
-
-// LOGIN CON FACEBOOK
+import {
+  providerFacebook,
+  providerGoogle,
+} from "../firebaseController/firebaseFunctions.js";
+import { irReglas } from "./redireccion.js";
 
 // creando cuenta de usuario
 export function registrar() {
-  const informacion = document.getElementsByClassName('formInformacion');
+  const informacion = document.getElementsByClassName("formInformacion");
 
-  document.addEventListener('click', (e) => {
-    if (e.target.matches('#btnR')) {
+  document.addEventListener("click", (e) => {
+    if (e.target.matches("#btnR")) {
       crearUsuario(informacion[0][1].value, informacion[0][2].value)
         .then(() => {
           // Signed in
-
           irReglas();
         })
         .catch((error) => {
           const errorMessage = error.message;
-          document.getElementById('errorRegistro').innerHTML = errorMessage;
+          document.getElementById("errorRegistro").innerHTML = errorMessage;
         });
     }
   });
@@ -28,30 +25,30 @@ export function registrar() {
 
 // registro con Google
 export function registroGoogle() {
-  const botonGoogle = document.getElementById('botonGoogle');
-
-  botonGoogle.addEventListener('click', (e) => {
-    e.preventDefault();
-    const provider = new firebase.auth.GoogleAuthProvider();
-    providerGoogle(provider)
-      .then(() => {
-        irReglas();
-      })
-      .catch(() => {
-      });
+  document.addEventListener("click", (e) => {
+    if (e.target.matches("#botonGoogle")) {
+      e.preventDefault();
+      const provider = new firebase.auth.GoogleAuthProvider();
+      providerGoogle(provider)
+        .then(() => {
+          irReglas();
+        })
+        .catch(() => {});
+    }
   });
 }
 
-/
-
-  botonFacebook.addEventListener('click', (e) => {
-    e.preventDefault();
-    const provider = new firebase.auth.FacebookAuthProvider();
-    providerFacebook(provider)
-      .then(() => {
-        irReglas();
-      })
-      .catch(() => {
-      });
+// registro con Facebook
+export function registroFacebook() {
+  document.addEventListener("click", (e) => {
+    if (e.target.matches("#botonFacebook")) {
+      e.preventDefault();
+      const provider = new firebase.auth.FacebookAuthProvider();
+      providerFacebook(provider)
+        .then(() => {
+          irReglas();
+        })
+        .catch(() => {});
+    }
   });
 }
