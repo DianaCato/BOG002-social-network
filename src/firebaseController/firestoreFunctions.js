e5562d3079f435637dc460d5ff66c6bec3b7f4b3
AngelicaT <76055451+AngelicaT319@users.noreply.github.com>
2021-05-06 21:41:54 -0500

    empezando boton editar

--- src/firebaseController/firestoreFunctions.js
+++ src/firebaseController/firestoreFunctions.js
@@ -1,10 +1,16 @@
 export default function savePost(title, description) {
   const db = firebase.firestore();
+  const taskForm = document.getElementById("task-form");
   const taskcotainer = document.getElementById("tasks-container");
   // borrar Post
   const borrarPost = (id) => db.collection("post").doc(id).delete();
-
-  db.collection("post").add({
+  // Editar Post
+  const editPost = (id) => db.collection("post").doc(id).get();
+  let editStatus = false;
+  let id = "";
+  const updateEdit = (id, updateEdit) => db.collection("post").doc(id).update(updateEdit);
+  // guardar Post
+  db.collection("post").doc().set({
     title,
     description,
   });
@@ -21,14 +27,36 @@ export default function savePost(title, description) {
           <h3>${tarea.title}</h3>
           <p>${tarea.description}</p>
           <button class=" btn btn-primario btn-borrar" data-id="${tarea.id}">Eliminar</button>
-          <button class="btn btn-secundario">Editar</button>
+          <button class="btn btn-secundario btn-edit" data-id="${tarea.id}">Editar</button>
           <div/>
           </form>`;
-
+      // Borrando Post
       const btnsBorrar = document.querySelectorAll(".btn-borrar");
       btnsBorrar.forEach((btn) => {
         btn.addEventListener("click", async (e) => {
           await borrarPost(e.target.dataset.id);
+        });
+      });
+      // Editando Post
+      const btnsEditar = document.querySelectorAll(".btn-edit");
+      btnsEditar.forEach((btn) => {
+        btn.addEventListener("click", async (e) => {
+          const doct = await editPost(e.target.dataset.id);
+          const tareas = doct.data();
+          editStatus = true;
+          id = doct.id;
+          taskForm.titulo.value = tareas.title;
+          taskForm.descripcion.value = tareas.description;
+          taskForm["btn-task-form"].innerText = "Editar";
+
+          if (!editStatus) {
+            await savePost(title, description);
+          } else {
+            await updateEdit(id, {
+              title: title,
+              description: description,
+            });
+          }
         });
       });
     });
